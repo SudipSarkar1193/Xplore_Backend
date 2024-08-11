bacdbe01d8897afaac7ec90f2e3a2761bfd98e0f
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2024-08-11 14:11:13 +0530

    Testing -02

--- middlewares/authenticateUser.middleware.js
+++ middlewares/authenticateUser.middleware.js
@@ -3,42 +3,41 @@ import { APIError } from "../utils/APIError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
+
 export const authenticateUser = asyncHandler(async (req, res, next) => {
 	try {
 		req.user = null;
 
+		// Extract access token from cookies or Authorization header
 		const accessToken =
 			req.cookies?.accessToken ||
 			req.header("Authorization")?.replace("Bearer ", "");
 
 		if (!accessToken) {
-			//throw new APIError(401, "Unauthorized Request");
-			next(new APIError(401, "Unauthorized Request"));
+			return next(new APIError(401, "Unauthorized Request"));
 		}
 
+		// Verify the JWT token
 		const verifiedToken = jwt.verify(
 			accessToken,
 			process.env.ACCESS_TOKEN_SECRET
 		);
 
-		if (!verifiedToken) {
-			//throw new APIError(401, "Unauthorized Request");
-			next(new APIError(401, "Unauthorized Request"));
+		// Find the user associated with the token
+		const user = await User.findById(verifiedToken._id).select(
+			"_id username email profileImg fullName"
+		);
+
+		if (!user) {
+			return next(new APIError(404, "User not found."));
 		}
 
-		const user = await User.findById(verifiedToken._id).select(
-			"_id username email profileImg username fullName"
-		);
-		if (!user) {
-			//	throw new APIError(404, "User not found.");
-			next(new APIError(404, "User not found..."));
-		}
+		// Attach the user to the request object
 		req.user = user;
 
 		next();
 	} catch (error) {
-		console.error("Authentication error : ", error);
-		//throw new APIError(500, error.message);
+		console.error("Authentication error:", error);
 		next(new APIError(500, error.message));
 	}
 });
