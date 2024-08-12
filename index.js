6448e40ae8eeb094c80063e33d755ee8c7d3f2c8
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2024-08-12 17:37:15 +0530

    Trying firebase - 06

--- index.js
+++ index.js
@@ -20,10 +20,14 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
+app.use((req, res, next) => {
+	res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+	next();
+});
 
 app.use(
 	cors({
-		origin: [process.env.CORS_ORIGIN,"http://localhost:5173"],
+		origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
 		credentials: true,
